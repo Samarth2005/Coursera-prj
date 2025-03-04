13854b2f257bbcdeadd72bef94bc74af8c0685cf
Cyphrus008 <96097892+Samarth2005@users.noreply.github.com>
2025-03-04 21:09:58 +0530

    simple-interest.sh script

---
+++ simple-interest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Script to calculate simple interest
+
+echo "Enter principal amount:"
+read principal
+echo "Enter rate of interest:"
+read rate
+echo "Enter time period (in years):"
+read time
+
+interest=$(echo "scale=2; ($principal * $rate * $time) / 100" | bc)
+echo "Simple Interest: $interest"
